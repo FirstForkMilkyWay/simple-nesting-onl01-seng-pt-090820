42d53468795eefef9ab9544a419ac973a89375a0
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-09-18 19:14:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -127,8 +127,7 @@ def adding_to_dennis
           :languages=> ["C"]
         }
      }
-   programmer_hash[:dennis_ritchie][:dennis_ritchie] =[:languages] 
-  programmer_hash[:dennis_ritchie][:languages] = ("Assembly")
+  [:dennis_ritchie][:languages][1]).to include("Assembly")
   programmer_hash
 
 end
